4ce642aab94f344e8c77fd9f88f5b8ed9ea25b9d
SolGee <solsv00@hotmail.com>
2019-08-28 22:37:46 -0500

    Starting Boiler plate

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const getPath = require('path');
+const getFs = require('fs');
+const getMarkdownIt = require('markdown-it');
+
+    md = new getMarkdownIt();
+
+
+
 module.exports = () => {
-  // ...
+  console.log('Funciona');
+  return 'Funciona'
 };
