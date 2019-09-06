06f67feb21feade8e2a06ad87382634a32c155a8
SolGee <solsv00@hotmail.com>
2019-09-06 14:36:18 -0500

    Reestructura cli.js, index.js, instalación de fetch nodejs

--- cli.js
+++ cli.js
@@ -1,15 +1,10 @@
 #!/usr/bin/env node
 
-const [A, B, ...args] = process.argv;
-
 const endPath = process.argv[2] || null;
-const validate = process.argv[3] || process.argv [4];
-const stats = process.argv[3] || process.argv [4];
 
-let statsStatus;
-stats === '--stats' ? statsStatus = true : statsStatus = null;
+const options = {
+    validate : process.argv.indexOf('--validate') > -1,
+    stats : process.argv.indexOf('--stats') > -1
+};
 
-let validateStatus;
-validate === '--validate' ? validateStatus = true : validateStatus = null;
-
-require('./index.js')(endPath, { validate: validateStatus, stats: statsStatus });
+require('./index.js')(endPath, options);
--- index.js
+++ index.js
@@ -2,35 +2,44 @@ const isMD = require('./lib/md.js');
 const readMarkdown = require('./lib/readMd.js');
 const filterMarkdown = require('./lib/filterFile.js');
 const statsOption = require('./lib/stats.js');
+const validateOption = require('./lib/validate.js');
 
 
 
 
 module.exports = (path, {validate, stats}) => {
-  
+
   if(path === null){
-    console.log('No se ha especificado ninguna ruta de archivo.')
+    console.log('No se ha especificado ninguna ruta de archivo.');
     return `No se ha especificado ninguna ruta de archivo.`;
   }
   if(isMD(path) === false){
-    console.log[('No hay ningun archivo MD en esta ruta.')]
-    return `No hay ningun archivo MD en esta ruta.`
+    console.log('No hay ningun archivo MD en esta ruta.');
+    return `No hay ningun archivo MD en esta ruta.`;
   }
   if(path){
     return readMarkdown(path)
     .then(data => {
       if(data === ''){
-        console.log('El archivo esta vacío')
+        console.log('El archivo esta vacío');
         return `El archivo esta vacío`;
       }
-      const justLinks = filterMarkdown(data, path)
+      const justLinks = filterMarkdown(data, path);
       if(stats){
+        console.log(statsOption(justLinks));
         return statsOption(justLinks);
+      } else if (validate){
+
+      } else if ( validate && stats){
+
+      } else {
+         console.log('No ingresaste una opción valida.\n Las banderas válidas son: \n --validate \n --stats.') 
+         return
       }
-      console.log(justLinks)
+      console.log(justLinks);
       return justLinks;
       
     })
-    .catch(error => console.log(error))
+    .catch(error => console.log(error));
   }
 };
